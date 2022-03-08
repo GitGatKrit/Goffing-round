4b226f6817ef19b316b752d932d79caa1ff05f24
GitGatKrit <101158338+GitGatKrit@users.noreply.github.com>
2022-03-07 20:41:22 -0600

    epic modile

--- main.lua
+++ main.lua
@@ -3,3 +3,10 @@ local goof = {}
 goof.GetDistance(p1, p2)
   return (p1 - p2).magnitude
 end
+goof.GetVector(p)
+  return Vector3.new(p.Position)
+end
+goof:Remove(table,index)
+  table.remove(table, table[index])
+end
+return goof
