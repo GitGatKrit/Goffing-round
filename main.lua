15a172567e1946381cb8f676043708cefdb17acc
GitGatKrit <101158338+GitGatKrit@users.noreply.github.com>
2022-03-07 20:42:09 -0600

    quite epic update

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 local goof = {}
 
-goof.GetDistance(p1, p2)
+goof.GetDistance = function(p1, p2)
   return (p1 - p2).magnitude
 end
-goof.GetVector(p)
+goof.GetVector = function(p)
   return Vector3.new(p.Position)
 end
-goof:Remove(table,index)
+goof:Remove = function(table,index)
   table.remove(table, table[index])
 end
 return goof
