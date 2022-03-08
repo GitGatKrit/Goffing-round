df8dfb665f55995e7087a323442fef9d90d2e9e1
GitGatKrit <101158338+GitGatKrit@users.noreply.github.com>
2022-03-07 20:47:42 -0600

    some fixed
    i dont know why you wold use this lol

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 local goof = {}
 
-goof.GetDistance = function(p1, p2)
+function goof.GetDistance(p1, p2)
   return (p1 - p2).magnitude
 end
-goof.GetVector = function(p)
+function goof.GetVector(p)
   return Vector3.new(p.Position)
 end
-goof:Remove = function(table,index)
+function goof:Remove(table,index)
   table.remove(table, table[index])
 end
 return goof
